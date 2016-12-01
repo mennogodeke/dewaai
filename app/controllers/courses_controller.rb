8ed887c1af7c3364ab741b3b35da06b3a5c36a28
Menno Godeke <mgodeke1994@gmail.com>
2016-12-01 15:28:13 +0100

    added some calculation stuff

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,9 +3,30 @@ class CoursesController < ApplicationController
   load_and_authorize_resource
   def index
     @courses = Course.all
+    @c1 = @courses.where(:course_type_id => 1)
+    @c2 = @courses.where(:course_type_id => 2)
+    @c3 = @courses.where(:course_type_id => 3)
     if user_signed_in?
       @entry = current_user.entries.build
     end
+    if can? :manage, :all
+      @c1.each do |c|
+        @c1entries = c.entries.count
+      end
+      @c2.each do |c|
+        @c2entries = c.entries.count
+      end
+      @c3.each do |c|
+        @c3entries = c.entries.count
+      end
+      @total1 = @c1entries * 500
+      @total2 = @c2entries * 700
+      @total3 = @c3entries * 800
+      @total = @total1 + @total2 + @total3
+    end
+
+
+
 
   end
 
@@ -13,6 +34,10 @@ class CoursesController < ApplicationController
     @course = Course.find(params[:id])
     @entry = current_user.entries.build
     @entries = Entry.where(:course_id => @course.id)
+
+    @entry_count = @course.entries.count
+    @totalspots = @course.course_type.design.capacity * @course.course_type.ship_amount
+    @freespots = @totalspots - @entry_count
   end
 
   def new
